55e806fbcd0be2b6d420d2ffdcbceff5252dbd35
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-01 23:33:50 +0500

    Fetch data successfully using axios with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 
-function App() {
-  return <div className="App">Create React Application using TypeScript</div>;
+interface IResponse {
+  id?: string;
+  author?: string;
+  width?: number;
+  height?: number;
+  url?: string;
+  download_url?: string;
 }
+
+const App: FC = () => {
+  const [apiResponse, setApiResponse] = useState<IResponse[]>([]);
+  const handleResponse = async () => {
+    const response = await axios.get<IResponse[]>(
+      'https://picsum.photos/v2/list'
+    );
+    const data1 = await response.data;
+    console.log('Data: ', data1);
+    setApiResponse(data1);
+  };
+
+  return (
+    <div className="App">
+      <button type="button" onClick={handleResponse}>
+        Click Me to Get Data
+      </button>
+      <section>
+        {apiResponse.map((value: IResponse, key) => (
+          <li key={key}>{value.author}</li>
+        ))}
+      </section>
+    </div>
+  );
+};
 
 export default App;
