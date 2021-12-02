58c85e3b6b47189a2d2730aa884eb83a0ca78551
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-02 21:28:07 +0500

    Write endpoints for the project

--- src/EndPoints.ts
+++ src/EndPoints.ts
@@ -2,10 +2,8 @@ const APP_ID = 'PmGqiCAY4CG3PZPHNcEG';
 
 export const INVOLVEMENT_API = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}`;
 
-export const POST_LIKE = `${INVOLVEMENT_API}/likes`; // item_id is required
-export const GET_LIKES = `${INVOLVEMENT_API}/likes`;
+export const LIKES = `${INVOLVEMENT_API}/likes`; // item_id is required
 
-export const POST_COMMENTS = `${INVOLVEMENT_API}/comments`; // item_id, username, comment is required
-export const GET_COMMENTS = `${INVOLVEMENT_API}/comments`;
+export const COMMENTS = `${INVOLVEMENT_API}/comments`; // item_id, username, comment is required
 
 export const GET_IMAGES_LIST = 'https://picsum.photos/v2/list';
