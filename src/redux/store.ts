38bcac4251e0d055c83b237ab6577d5873cc789f
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-05 01:01:38 +0500

    Remove unnecessary import

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from './reducers/RootReducer';
-import LoremPicsumReducer from './reducers/LoremPicsumReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
